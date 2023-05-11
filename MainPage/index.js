e0f313997c0a0ba26364afbc0876c8247da00c81
Bai Omar <bai.laye22@bathspa.ac.uk>
2023-05-11 15:46:20 +0100

    Home page is completed with no known bug... so far anyway

--- MainPage/index.js
+++ MainPage/index.js
@@ -1,7 +1,22 @@
-var navbuttons = document.getElementById('StickyHeader');
+console.log("JS C0nnected");
+SwitchDesc("Dragons")
 
-window.onscroll = function (e) {  
-if(navbuttons.scrollY == 0) {
-    alert("hishf");
-}
-}
+function SwitchDesc(NewDesc) {
+    var i;
+    var AllDesc = document.getElementsByClassName("MK_Desc");
+    for (i = 0; i < AllDesc.length; i++) {
+        AllDesc[i].style.display = "none"; 
+    }
+    document.getElementById(NewDesc).style.display = "block";
+    
+
+    var Allbtns = document.getElementsByClassName("IPNbtn");
+
+    console.log(Allbtns)
+    let btnNewDesc = "btn" + NewDesc;
+    console.log(btnNewDesc);
+    for (let i = 0; i < Allbtns.length; i++) {
+        Allbtns[i].classList.remove("InPageNavSel");
+    }
+    document.getElementById(btnNewDesc).classList.add("InPageNavSel");
+  }
