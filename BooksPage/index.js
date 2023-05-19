cf16e4754d1d62a42ebd58d61f56a8c4a336fe09
Bai Omar Laye <bai.laye22@bathspa.ac.uk>
2023-05-19 17:49:13 +0100

    Ill continue on this later

--- BooksPage/index.js
+++ BooksPage/index.js
@@ -1,1 +1,9 @@
 console.log("JS C0nnected");
+
+Frames = document.getElementsByClassName("Frame");
+
+for (let i = 0; i < Frames.length; i++) {
+    if (1 == i%2) {
+        Frames[i].style.marginLeft = "10vw"
+    }
+}
