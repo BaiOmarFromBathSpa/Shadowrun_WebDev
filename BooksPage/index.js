dd64b5ac70a3e6921e052a9952fad32442235c54
Bai Omar Laye <bai.laye22@bathspa.ac.uk>
2023-05-19 17:49:38 +0100

    slight tweaks i forgot to save

--- BooksPage/index.js
+++ BooksPage/index.js
@@ -4,6 +4,9 @@ Frames = document.getElementsByClassName("Frame");
 
 for (let i = 0; i < Frames.length; i++) {
     if (1 == i%2) {
-        Frames[i].style.marginLeft = "10vw"
+        Frames[i].style.marginLeft = "8vw"
+    }
+    else {
+        Frames[i].style.marginLeft = "42vw"
     }
 }
