87ac03ba97a2c03dc410f75e9a1e7d0347eb2baf
Bai Omar Laye <bai.laye22@bathspa.ac.uk>
2023-05-23 10:53:41 +0100

    image frames added

--- BooksPage/index.js
+++ BooksPage/index.js
@@ -1,12 +1,15 @@
 console.log("JS C0nnected");
 
 Frames = document.getElementsByClassName("Frame");
+BookCov = document.getElementsByClassName("BookCov");
 
 for (let i = 0; i < Frames.length; i++) {
     if (1 == i%2) {
-        Frames[i].style.marginLeft = "8vw"
+        Frames[i].style.marginLeft = "8vw";
+        BookCov[i].style.left = "63vw";
+        BookCov[i].style.transform = "rotate(-5deg)";
     }
     else {
-        Frames[i].style.marginLeft = "42vw"
+        Frames[i].style.marginLeft = "42vw";
     }
 }
