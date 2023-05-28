4ef5a5fc428618022a0a3d9f13cc0d61f2e535fd
Bai Omar Laye <bai.laye22@bathspa.ac.uk>
2023-05-28 13:14:04 +0100

    Main Page Commented

--- MainPage/index.js
+++ MainPage/index.js
@@ -1,15 +1,17 @@
-console.log("JS C0nnected");
-SwitchDesc("Dragons")
+console.log("JS C0nnected"); // pls ignore i find it fun
+SwitchDesc("Dragons") //Starts with Dragons, can swtch it tho
 
 function SwitchDesc(NewDesc) {
-    var i;
-    var AllDesc = document.getElementsByClassName("MK_Desc");
-    for (i = 0; i < AllDesc.length; i++) {
+    var i; /* Used part of the code from W3C and for some reason they put the iterator outside the for loop */
+    var AllDesc = document.getElementsByClassName("MK_Desc"); //colects the discriptions into an array
+    for (i = 0; i < AllDesc.length; i++) {  // Disables all discriptions
         AllDesc[i].style.display = "none"; 
     }
-    document.getElementById(NewDesc).style.display = "block";
+    document.getElementById(NewDesc).style.display = "block"; //And Enables the one passed in as a parameter
     
 
+    // the code below does the same as the descriptions fbut for the grid of buttons instead
+    // Note: Doesnt disable them just makes the parameter one light up 
     var Allbtns = document.getElementsByClassName("IPNbtn");
 
     console.log(Allbtns)
